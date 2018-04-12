1869d9d2993a9701388cd973e0129263a72b3b5d
GaoChengquan <529390053@qq.com>
2018-04-12 11:16:10 +0800

    init

---
+++ src/main/java/com/situ/ssh/controller/AdminAction.java
@@ -0,0 +1,11 @@
+package com.situ.ssh.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+public class AdminAction extends ActionSupport{
+
+	
+}
